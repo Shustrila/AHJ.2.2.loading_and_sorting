31010463d64164d563464feef0990feea9ec9373
Александр Махалов <aleksandrmahalov@MacBook-Aleksandr.local>
2019-04-25 12:02:46 +0300

    fix

--- src/js/Table.js
+++ src/js/Table.js
@@ -1,35 +1,90 @@
 class Table {
   constructor(el, data) {
-    this.el = document.querySelector(el);
+    this.el = el;
     this.data = data;
+    this.selectAttr = 0;
+    this.selectData = ['id', 'title', 'year', 'imdb'];
+    this.ascending = true;
     this.counter = 0;
 
-    this.createRows();
+    this._createRows();
+  }
+
+  static generateArrow(nameAttr, code) {
+    const head = document.querySelector(`[data-name-head=${nameAttr}]`);
+    const arrowSort = document.querySelector('.js-arrow-sort');
+    const span = document.createElement('span');
+
+    if (arrowSort !== null) {
+      arrowSort.remove();
+    }
+
+    span.className = 'js-arrow-sort';
+    span.innerHTML = code;
+
+    head.appendChild(span);
   }
 
   sortBy() {
-    if (this.data[this.counter + 1] !== undefined) {
-      const arrowSort = document.querySelector('#arrow-sort');
-      const dataId = this.el.querySelectorAll('[data-id]');
-      const thisAttrId = dataId[this.counter].getAttribute('data-id');
-      const nextAttrId = dataId[this.counter + 1].getAttribute('data-id');
+    const el = document.querySelector(this.el);
+    const { children } = el;
+    const dataAttr = this.selectData[this.selectAttr];
+    let childrenAttr = children[this.counter].dataset[dataAttr];
+    const testNumber = /\d/i.test(childrenAttr[0]);
 
-      if (+thisAttrId > +nextAttrId) {
-        arrowSort.innerHTML = '&#x2191';
-        this.el.insertBefore(dataId[this.counter], this.el.children[this.counter + 1]);
-        this.el.insertBefore(dataId[this.counter + 1], this.el.children[this.counter]);
-      } else {
-        arrowSort.innerHTML = '&#x2193';
+    if (this.ascending && this.counter + 1 < this.data.length) {
+      let nextChildrenAttr = children[this.counter + 1].dataset[dataAttr];
+
+      Table.generateArrow(dataAttr, '&#x2193');
+
+      if (testNumber) {
+        childrenAttr = +childrenAttr;
+        nextChildrenAttr = +nextChildrenAttr;
+      }
+
+      if (childrenAttr > nextChildrenAttr) {
+        el.insertBefore(children[this.counter + 1], el.children[this.counter]);
+        el.insertBefore(children[this.counter], el.children[this.counter + 1]);
+      }
+
+      this.counter += 1;
+      if (this.counter + 1 === this.data.length) this.ascending = false;
+    } else {
+      let prevChildrenAttr = children[this.counter - 1].dataset[dataAttr];
+
+      Table.generateArrow(dataAttr, '&#x2191');
+
+      if (testNumber) {
+        childrenAttr = +childrenAttr;
+        prevChildrenAttr = +prevChildrenAttr;
+      }
+
+      if (childrenAttr > prevChildrenAttr) {
+        el.insertBefore(children[this.counter], el.children[this.counter - 1]);
+        el.insertBefore(children[this.counter - 1], el.children[this.counter]);
+      }
+
+      this.counter -= 1;
+      if (this.counter === 0) {
+        this.selectAttr += 1;
+        if (this.selectAttr === this.selectData.length) this.selectAttr = 0;
+        this.ascending = true;
       }
     }
-    this.counter++;
   }
 
-  createRows() {
+  _createRows() {
+    const el = document.querySelector(this.el);
+
     for (const row of this.data) {
       const tr = document.createElement('tr');
 
-      tr.innerHTML = `<td>#${row.id}</td><td>${row.title}</td><td>(${row.year})</td><td>imdb: ${row.imdb}</td>`;
+      tr.innerHTML = `
+        <td>${row.id}</td>
+        <td>${row.title}</td>
+        <td>(${row.year})</td>
+        <td>imdb: ${row.imdb}</td>
+      `;
 
       tr.className = 'js-table-tr';
       tr.setAttribute('data-id', row.id);
@@ -37,7 +92,7 @@ class Table {
       tr.setAttribute('data-year', row.year);
       tr.setAttribute('data-imdb', row.imdb);
 
-      this.el.appendChild(tr);
+      el.appendChild(tr);
     }
   }
 }
